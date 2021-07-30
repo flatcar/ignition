e796530766f841f1b46b86f81262d0bf076f162f
Mathieu Tortuyaux <mathieu@kinvolk.io>
2021-07-30 14:21:31 +0200

    internal/prv/cmdline: backport flatcar patche
    this patch provides backward compatiblity for various config
    key
    
    Signed-off-by: Mathieu Tortuyaux <mathieu@kinvolk.io>

--- internal/providers/cmdline/cmdline.go
+++ internal/providers/cmdline/cmdline.go
@@ -33,7 +33,9 @@ import (
 )
 
 const (
-	cmdlineUrlFlag = "ignition.config.url"
+	cmdlineUrlFlagLegacyCoreOS = "coreos.config.url"
+	cmdlineUrlFlagLegacy       = "flatcar.config.url"
+	cmdlineUrlFlag             = "ignition.config.url"
 )
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
@@ -82,7 +84,7 @@ func parseCmdline(cmdline []byte) (url string) {
 		parts := strings.SplitN(strings.TrimSpace(arg), "=", 2)
 		key := parts[0]
 
-		if key != cmdlineUrlFlag {
+		if !(key == cmdlineUrlFlagLegacy || key == cmdlineUrlFlagLegacyCoreOS || key == cmdlineUrlFlag) {
 			continue
 		}
 
